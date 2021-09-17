31552165c67e6720d9925b6bf7eca76b5988f1bd
Michaël Martens <24@24degrees.be>
2021-09-17 11:38:54 +0200

    Ready for testing frontend.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -68,6 +68,9 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('chatify') }}">
+                                {{ __('Chatify') }}
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">

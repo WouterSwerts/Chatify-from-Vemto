05fa36f1498531485cf67658fe018c5f3b5e7950
Michaël Martens <24@24degrees.be>
2021-09-17 14:02:13 +0200

    Stripped Chat from ugly code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,47 +26,39 @@
 
         <div class="col-md-4">
 
-            <div class="content">
-                <div class="row">
-
-                    <div class="">
-                        <div class="card">
-                            <div class="card-header">
-                                Chat with USER
-                                <button type="button" 
-                                class="btn btn-sm btn-outline-primary float-right">
-                                    Continue
-                                </button>
+            <div class="card">
+                <div class="card-header">
+                    Chat with USER
+                    <button type="button" 
+                    class="btn btn-sm btn-outline-primary float-right">
+                        Continue
+                    </button>
+                </div>
+                <div class="card-body height3">
+                    <ul class="chat-list">
+                        <li class="in">
+                            <div class="img-fluid chat-img">
+                                <img alt="Avtar" src="https://bootdey.com/img/Content/avatar/avatar1.png">
                             </div>
-                            <div class="card-body height3">
-                                <ul class="chat-list">
-                                    <li class="in">
-                                        <div class="img-fluid chat-img">
-                                            <img alt="Avtar" src="https://bootdey.com/img/Content/avatar/avatar1.png">
-                                        </div>
-                                        <div class="chat-body">
-                                            <div class="chat-message">
-                                                <h5>Jimmy Willams</h5>
-                                                <p>Raw denim heard of them tofu master cleanse</p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="out">
-                                        <div class="img-fluid chat-img">
-                                            <img alt="Avtar" src="https://bootdey.com/img/Content/avatar/avatar6.png">
-                                        </div>
-                                        <div class="chat-body">
-                                            <div class="chat-message">
-                                                <h5>Serena</h5>
-                                                <p>Next level veard</p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
+                            <div class="chat-body">
+                                <div class="chat-message">
+                                    <h5>Jimmy Willams</h5>
+                                    <p>Raw denim heard of them tofu master cleanse</p>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-
+                        </li>
+                        <li class="out">
+                            <div class="img-fluid chat-img">
+                                <img alt="Avtar" src="https://bootdey.com/img/Content/avatar/avatar6.png">
+                            </div>
+                            <div class="chat-body">
+                                <div class="chat-message">
+                                    <h5>Serena</h5>
+                                    <p>Next level veard</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
 
